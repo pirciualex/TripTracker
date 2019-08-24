a9a880877f6cfb865fe4b2c06a74a9056b5f98bc
Alex Pirciu <alex.pirciu@zohomail.eu>
2019-08-24 03:44:31 +0300

    Trip service interface added

---
+++ src/TripTracker.API/Services/ITripService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TripTracker.API.Models;
+
+namespace TripTracker.API.Services
+{
+    public interface ITripService
+    {
+        IEnumerable<Trip> Get();
+        Trip GetById(int id);
+        Task Add(Trip trip);
+        Task Update(Trip trip);
+        Task Delete(int id);
+    }
+}
