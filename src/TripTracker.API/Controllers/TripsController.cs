f9f61d11c0e141644207e4ec3b0970e4d9196886
Alex Pirciu <alex.pirciu@zohomail.eu>
2019-08-24 04:11:10 +0300

    Trip controller added with CRUD actions

---
+++ src/TripTracker.API/Controllers/TripsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TripTracker.API.Models;
+using TripTracker.API.Services;
+
+namespace TripTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        private readonly ITripService _tripService;
+
+        public TripsController(ITripService tripService)
+        {
+            _tripService = tripService;
+        }
+
+        // GET: api/Trip
+        [HttpGet]
+        public IEnumerable<Trip> Get()
+        {
+            return _tripService.Get();
+        }
+
+        // GET: api/Trip/5
+        [HttpGet("{id}", Name = "Get")]
+        public Trip Get(int id)
+        {
+            return _tripService.GetById(id);
+        }
+
+        // POST: api/Trip
+        [HttpPost]
+        public void Post([FromBody] Trip trip)
+        {
+            _tripService.Add(trip);
+        }
+
+        // PUT: api/Trip/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Trip trip)
+        {
+            _tripService.Update(trip);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _tripService.Delete(id);
+        }
+    }
+}
--- src/TripTracker.API/Startup.cs
+++ src/TripTracker.API/Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using TripTracker.API.Services;
 
 namespace TripTracker.API
 {
@@ -25,6 +19,8 @@ namespace TripTracker.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ITripService, TripRepository>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
