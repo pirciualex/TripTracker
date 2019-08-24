e7833873ae291f60c1d56dd16c9d3a722dc058f1
Alex Pirciu <alex.pirciu@zohomail.eu>
2019-08-24 03:34:19 +0300

    Model classes added

---
+++ src/TripTracker.API/Models/Trip.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TripTracker.API.Models
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+    }
+}
