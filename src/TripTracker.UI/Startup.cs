2af82dd1913a4b3f4f7df50118452b7371f90c8a
Alex Pirciu <alex.pirciu@zohomail.eu>
2019-08-31 01:16:37 +0300

    Api client configured in Startup

--- src/TripTracker.UI/Startup.cs
+++ src/TripTracker.UI/Startup.cs
@@ -1,18 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using TripTracker.UI.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using TripTracker.API.Services;
+using TripTracker.UI.Data;
+using TripTracker.UI.Services;
 
 namespace TripTracker.UI
 {
@@ -28,6 +27,13 @@ namespace TripTracker.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(Configuration["serviceUrl"])
+            };
+            services.AddSingleton(httpClient);
+            services.AddSingleton<ITripService, ApiClient>();
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
